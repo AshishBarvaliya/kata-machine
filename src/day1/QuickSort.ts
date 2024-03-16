e2ff15702f49aba63d1ce5c3b0e05f5279d75183
Ashish Baravaliya <vier.institute35@gmail.com>
2024-03-16 14:07:41 -0400

    till linkedlist

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,34 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    let pivotIdx = partition(arr, low, high);
+
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    let pivot = arr[high];
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            let temp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
